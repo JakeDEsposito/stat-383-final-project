4d4147857fc830078b2d1de855e16134517be588
JakeDEsposito <97902257+JakeDEsposito@users.noreply.github.com>
2023-12-07 21:37:24 -0500

    Removed data_culled

--- main.R
+++ main.R
@@ -8,18 +8,11 @@ library("readxl")
 # Read data from file
 spotify_data <- read.csv("./spotify-2023.csv")
 
-# Culled data to only include necessary variables.
-# Fix for streams being assumed to be characters and not numeric.
-data_culled <- data.frame(
-  bpm = spotify_data$bpm,
-  streams = as.numeric(spotify_data$streams)
-)
-
-fit_lm <- lm(bpm ~ streams, data_culled)
+fit_lm <- lm(bpm ~ streams, spotify_data)
 print(summary(fit_lm))
 
 # Plotting the data points
-plot(data_culled$bpm, data_culled$streams, main = "Streams vs BPM",
+plot(spotify_data$streams, spotify_data$bpm, main = "Streams vs BPM",
      xlab = "BPM", ylab = "Streams",
      pch = 19)
 
@@ -27,5 +20,5 @@ plot(data_culled$bpm, data_culled$streams, main = "Streams vs BPM",
 abline(fit_lm, col = "blue")
 
 # Chi Squared Test
-chi_squared_test <- chisq.test(unlist(x = data_culled$streams), unlist(x = data_culled$bpm), TRUE)
+chi_squared_test <- chisq.test(unlist(x = spotify_data$streams), unlist(x = spotify_data$bpm), TRUE)
 print(chi_squared_test)
