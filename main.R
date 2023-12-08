7aeca9d776ed401bdf8b5a68e07f8e93d77983df
JakeDEsposito <97902257+JakeDEsposito@users.noreply.github.com>
2023-12-07 21:04:23 -0500

    Added goodness of fit code to main.R

--- main.R
+++ main.R
@@ -1,3 +1,27 @@
-# Read data from file
-data <- read.csv("./spotify-2023.csv")
+cat("\f") # clean the console
+rm(list = ls()) # clean the environment
 
+# Install and import readxl
+install.packages("readxl")
+library("readxl")
+
+# Read data from file
+spotify_data <- read.csv("./spotify-2023.csv")
+
+# Culled data to only include necessary variables.
+# Fix for streams being assumed to be characters and not numeric.
+data_culled <- data.frame(
+  bpm = spotify_data$bpm,
+  streams = as.numeric(spotify_data$streams)
+)
+
+fit_lm <- lm(bpm ~ streams, data_culled)
+print(summary(fit_lm))
+
+# Plotting the data points
+plot(data_culled$bpm, data_culled$streams, main = "Streams vs BPM",
+     xlab = "BPM", ylab = "Streams",
+     pch = 19)
+
+# Adding the regression line to the above plot
+abline(fit_lm, col = "blue")
